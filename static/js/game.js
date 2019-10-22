92e2e2080e986df3016364924e4da9d7777b0cf1
Krisztian Pometko <krisztian@pometko.hu>
2019-10-22 13:11:21 +0200

    Added funtion for counting neighbouring cells

--- static/js/game.js
+++ static/js/game.js
@@ -18,9 +18,27 @@ function initGameState(){
         for (let y =0; y < height; y++){
             state[x][y] = new Cell(0, "none");
         }
-
     }
     return state;
 }
+
+
+function getNeighbourCount(x,y, gameState){
+    let neighbourCount = 0;
+    for (let xOffset = -1; xOffset <= 1;xOffset++){
+        for (let yOffset = -1; yOffset <= 1;yOffset++) {
+            if (xOffset != 0 &&
+                yOffset != 0 &&
+                gameState[x+xOffset][y+yOffset] &&
+                gameState[x+xOffset][y+yOffset] != 0){
+                    neighbourCount++;
+            }
+        }
+    }
+    return neighbourCount;
+}
+
+
+
 
 gameState = initGameState();
